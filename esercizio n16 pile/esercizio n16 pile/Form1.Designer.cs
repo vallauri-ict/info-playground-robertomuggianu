a13fd4442f61647c1f610681d4ec912174c30654
robertomuggianu <62752767+robertomuggianu@users.noreply.github.com>
2020-11-19 17:45:45 +0100

    Add files via upload

---
+++ esercizio n16 pile/esercizio n16 pile/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace esercizio_n16_pile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAggiungi = new System.Windows.Forms.Button();
+            this.btnEstrai = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codice:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 28);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Peso:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(204, 28);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 8);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tara:";
+            // 
+            // btnAggiungi
+            // 
+            this.btnAggiungi.Location = new System.Drawing.Point(11, 100);
+            this.btnAggiungi.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAggiungi.Name = "btnAggiungi";
+            this.btnAggiungi.Size = new System.Drawing.Size(118, 38);
+            this.btnAggiungi.TabIndex = 6;
+            this.btnAggiungi.Text = "Aggiungi Container Alla Pila";
+            this.btnAggiungi.UseVisualStyleBackColor = true;
+            this.btnAggiungi.Click += new System.EventHandler(this.btnAggiungi_Click);
+            // 
+            // btnEstrai
+            // 
+            this.btnEstrai.Location = new System.Drawing.Point(162, 100);
+            this.btnEstrai.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEstrai.Name = "btnEstrai";
+            this.btnEstrai.Size = new System.Drawing.Size(118, 38);
+            this.btnEstrai.TabIndex = 7;
+            this.btnEstrai.Text = "Estrai Container Dalla Pila";
+            this.btnEstrai.UseVisualStyleBackColor = true;
+            this.btnEstrai.Click += new System.EventHandler(this.btnEstrai_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 241);
+            this.Controls.Add(this.btnEstrai);
+            this.Controls.Add(this.btnAggiungi);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Container";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAggiungi;
+        private System.Windows.Forms.Button btnEstrai;
+    }
+}
+
---
+++ esercizio n16 pile/esercizio n16 pile/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace esercizio_n16_pile
+{
+    public partial class Form1 : Form
+    {
+        public struct Containers
+        {
+            public int codice;
+            public int peso;
+            public int tara;
+        }
+        Stack<Containers> pila = new Stack<Containers>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAggiungi_Click(object sender, EventArgs e)
+        {
+            Containers container;
+            container.codice = Convert.ToInt32(textBox1.Text);
+            container.peso = Convert.ToInt32(textBox2.Text);
+            container.tara = Convert.ToInt32(textBox3.Text);
+
+            pila.Push(container);
+            MessageBox.Show("elemento inserito nello stack");
+        }
+
+        private void btnEstrai_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Containers estratto = pila.Pop();
+                MessageBox.Show("codice: "+estratto.codice+"\npeso: "+estratto.peso + "\nTara" +estratto.tara);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("nessuna pila");
+            }
+        }
+    }
+}
